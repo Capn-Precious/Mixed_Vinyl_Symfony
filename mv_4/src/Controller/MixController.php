d8a354a4233e5d774c9c13ce89512e782e8469df
Precious Smith <precious.smith@pmg.com>
2022-09-30 16:59:45 -0500

    query for a single entry show page

--- mv_4/src/Controller/MixController.php
+++ mv_4/src/Controller/MixController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MixController extends AbstractController
 {
-    #[Route('/mix/new')]
+    #[Route('/mix/new', name: 'app_new')]
     public function new(EntityManagerInterface $entityManager): Response
     {
         $mix = new VinylMix();
@@ -29,5 +30,16 @@ class MixController extends AbstractController
             $mix->getId(),
             $mix->getTrackCount()
         ));
+    }
+
+    #[Route('/mix/{id}', name: 'app_mix_show')]
+    public function show($id, VinylMixRepository $mixRepository)
+    {
+        $mix = $mixRepository->find($id);
+
+
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix
+        ]);
     }
 }
